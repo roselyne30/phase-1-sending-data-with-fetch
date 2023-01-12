0f36b56627da29ffcc9d2954b3199005325e77bf
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2023-01-12 22:54:43 +0300

    My Complete Assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+// // Add your code here
+// function submitData(username, useremail)
+
+//     fetch('http://localhost:3000/users', {
+//       method: "POST",
+//       Headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({username, useremail})
+//     })
+//     .then(function (response) {
+//         return response.json();
+//       })
+//       //.then function (data)
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      Accept: 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({name, email
+    })
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(object){
+    document.body.innerHTML=body["id"]
+  })
+  .catch(function(error) {
+    document.body.innerHTML=error.message
+  })
+  }
